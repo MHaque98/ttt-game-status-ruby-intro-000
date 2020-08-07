b096ddd63262ecee472073de2320328c245437fc
Mahmud H <mahmud_haque@hotmail.co.uk>
2020-08-07 14:58:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,14 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
+  [0, 1, 2], # Top row
+  [3, 4, 5], # Middle row
+  [6, 7, 8], # Bottom row
+  [0, 3, 6], # Left vertical
+  [1, 4, 7], # Middle vertical
+  [2, 5, 8], # Right vertical
+  [0, 4, 8], # Upper left diagonal
+  [2, 4, 6] # Upper right diagonal
 ]
 
 def won?(board)
