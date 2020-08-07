e019dae8092c4d684fb715fd3de5dc78cd504f7e
Mahmud H <mahmud_haque@hotmail.co.uk>
2020-08-07 14:55:52 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,8 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
      return win_combo
+   elsif (board[win_index1] == "O" && board[win_index2] == "O" && board[win_index3] == "O")
+    return win_combo
    else
      return false
    end
